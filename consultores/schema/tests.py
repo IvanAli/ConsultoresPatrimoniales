ba622397deb2ae84f51a3b1be5ac778a55a9aabd
IvanAli <ivanali@outlook.com>
2015-10-18 23:40:59 -0500

    test para probar que los tests funcionan (je)

--- consultores/schema/tests.py
+++ consultores/schema/tests.py
@@ -1,3 +1,15 @@
+# Testeo de la pagina home
+# Author: Ivan
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from .models import Agente
 
-# Create your tests here.
+class TestHomePage(TestCase):
+    def test_uses_index_template(self):
+        response = self.client.get(reverse("schema:home"))
+        self.assertTemplateUsed(response, "schema/index.html")
+
+    def test_uses_base_template(self):
+        response = self.client.get(reverse("schema:home"))
+        self.assertTemplateUsed(response, "base.html")
